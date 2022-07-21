5a7f819fd5b98efd976ecf3ac4120fb5ef3e2a98
Vlad Legkiy <103931385+Vlad21anon@users.noreply.github.com>
2022-07-21 19:35:31 +0300

    fizzString2
    Given an int n, return the string form of the number followed by "!". So the int 6 yields "6!". Except if the number is divisible by 3 use "Fizz" instead of the number, and if the number is divisible by 5 use "Buzz", and if divisible by both 3 and 5, use "FizzBuzz". Note: the % "mod" operator computes the remainder after division, so 23 % 10 yields 3. What will the remainder be when one number divides evenly into another? (See also: FizzBuzz Code and Introduction to Mod)
    fizzString2(1) → "1!"
    fizzString2(2) → "2!"
    fizzString2(3) → "Fizz!"

---
+++ Logic1/fizzString2.java
@@ -0,0 +1,15 @@
+package CodingBat.Logic1;
+
+public class fizzString2 {
+    public static void main(String[] args) {
+        System.out.println(fizzString2(100));
+    }
+
+    public static String fizzString2(int n) {
+        if(n%3==0&&n%5==0) return "FizzBuzz!";
+        else if(n%3==0) return "Fizz!";
+        else if(n%5==0) return "Buzz!";
+        return Integer.toString(n) + "!";
+    }
+
+}
