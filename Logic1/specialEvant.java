568c229252873deb2ad24fe94f5363b7584c9303
Vlad Legkiy <103931385+Vlad21anon@users.noreply.github.com>
2022-07-21 19:01:49 +0300

    specialEleven
    We'll say a number is special if it is a multiple of 11 or if it is one more than a multiple of 11. Return true if the given non-negative number is special. Use the % "mod" operator -- see Introduction to Mod
    specialEleven(22) → true
    specialEleven(23) → true
    specialEleven(24) → false

---
+++ Logic1/specialEvant.java
@@ -0,0 +1,12 @@
+package CodingBat.Logic1;
+
+public class specialEvant {
+    public static void main(String[] args) {
+        System.out.println(specialEleven(10));
+    }
+
+    public static boolean specialEleven(int n) {
+        if(n%11==0 || n%11==1) return true;
+        return false;
+    }
+}
