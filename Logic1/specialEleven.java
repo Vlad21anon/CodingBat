2031117ca612e6b1bd0b8d86774a4b90cfa2cb20
Vlad Legkiy <103931385+Vlad21anon@users.noreply.github.com>
2022-07-21 19:03:02 +0300

    Renamed
    There was an error in the name renamed

--- Logic1/specialEleven.java
+++ Logic1/specialEleven.java
@@ -1,6 +1,6 @@
 package CodingBat.Logic1;
 
-public class specialEvant {
+public class specialEleven {
     public static void main(String[] args) {
         System.out.println(specialEleven(10));
     }
